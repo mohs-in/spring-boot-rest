772f3fd8912f165c8392ec0b39c6f7f386d5d221
Mohsin <in.mohsin@outlook.com>
2024-07-02 23:26:46 +0530

    init commit

---
+++ src/main/java/com/telusko/springbootrest/controller/JobRestController.java
@@ -0,0 +1,48 @@
+package com.telusko.springbootrest.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.telusko.springbootrest.model.JobPost;
+import com.telusko.springbootrest.service.JobService;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000" )
+public class JobRestController {
+	
+	@Autowired
+	private JobService service;
+	
+	
+	@GetMapping("jobPosts")
+	public List<JobPost> getAllJobs() {
+		return service.getAllJobs();
+	}
+
+	@GetMapping("jobPost/{postId}")
+	public JobPost getJob(@PathVariable int postId) {
+		return service.getJob(postId);
+	}
+
+	@PostMapping("jobPost")
+	public JobPost addJobPost(@RequestBody JobPost jobPost) {
+		service.addJobPost(jobPost);
+		return service.getJob(jobPost.getPostId());
+	}
+
+	@PutMapping("jobPost")
+	public JobPost updateJob(@RequestBody JobPost jobPost) {
+		return service.updateJob(jobPost);
+	}
+
+	@DeleteMapping("jobPost/{postId}")
+	public String deleteJob(@PathVariable int postId) {
+
+		service.deleteJob(postId);
+		return "Deleted";
+
+	}
+
+}
